828549842ec1cec99ddd2de411f620f995af1cea
Adrien Brignon <contact@abrignon.dev>
2019-05-09 21:27:05 +0200

    git subrepo pull (merge) lib/libredcode
    subrepo:
      subdir:   "lib/libredcode"
      merged:   "67bb184"
    upstream:
      origin:   "https://git.abrignon.dev/adrienbrignon/libredcode"
      branch:   "master"
      commit:   "67bb184"
    git-subrepo:
      version:  "0.4.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "5d6aba9"

---
+++ lib/libredcode/include/config.h
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2018
+** libredcode
+** File description:
+** The redcode configuration.
+*/
+
+#ifndef REDCODE_CONFIG_H
+#define REDCODE_CONFIG_H
+
+#define REDCODE_HEADER (0xf383ea00)
+
+#define MAX_ARGS (4)
+
+#define IND_SIZE (2)
+
+#define REG_SIZE (1)
+#define REG_CHAR ('r')
+#define REG_COUNT (16)
+
+#define DIR_SIZE (4)
+#define DIR_CHAR ('%')
+
+#define LAB_CHAR (':')
+#define COMMENT_CHAR ('#')
+#define SEPARATOR_CHAR (',')
+
+#define LAB_CHARS ("abcdefghijklmnopqrstuvwxyz_0123456788")
+
+#define NAME_STR (".name")
+#define NAME_LENGTH (128)
+
+#define COMMENT_STR (".comment")
+#define COMMENT_LENGTH (2048)
+
+#endif
--- lib/libredcode/include/redcode.h
+++ lib/libredcode/include/redcode.h
@@ -5,11 +5,57 @@
 ** The library main header.
 */
 
-#include <stdio.h>
-
 #ifndef REDCODE_H
 #define REDCODE_H
 
-int redcode_encode(const FILE *src, const FILE *dst);
+#include <stdio.h>
+#include <stddef.h>
+
+#include "config.h"
+
+typedef enum type {
+    T_NONE = 0,
+    T_REG = 1 << 0,
+    T_DIR = 1 << 1,
+    T_IND = 1 << 2,
+    T_LAB = 1 << 3,
+    T_NAME = 1 << 4,
+    T_COMMENT = 1 << 5
+} type_t;
+
+typedef struct parser {
+    off_t offset;
+    size_t size;
+    FILE *src;
+    FILE *dest;
+} parser_t;
+
+typedef struct argument {
+    type_t type;
+    size_t size;
+    const void *value;
+} argument_t;
+
+typedef struct token {
+    const char *name;
+    size_t args;
+    unsigned int type[MAX_ARGS];
+    unsigned char code;
+    size_t cycles;
+} token_t;
+
+int redcode_encode(FILE *src, FILE *dst);
+
+int parse_name(parser_t *parser);
+int encode_name(parser_t *parser, char *str, FILE *dst);
+int parse_comment(parser_t *parser);
+int encode_comment(parser_t *parser, char *str, FILE *dst);
+int parse_size(parser_t *parser);
+
+ssize_t readfile(FILE *fp, char **ptr);
+
+size_t redcode_write(parser_t *p, const void *ptr, size_t size, size_t nmemb);
+
+const token_t *get_token(const char *str);
 
 #endif
