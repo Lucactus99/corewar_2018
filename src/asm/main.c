3be8c263bd6218ea3a85d996c6678da3ee148e43
Adrien Brignon <contact@abrignon.dev>
2019-05-12 19:41:43 +0200

    Updated main to use new redcode library API

--- src/asm/main.c
+++ src/asm/main.c
@@ -10,21 +10,37 @@
 #include "corewar.h"
 #include "redcode.h"
 
+static FILE *get_input(const char *name)
+{
+    FILE *in = NULL;
+
+    if ((in = fopen(name, "r")) == NULL)
+        if ((in = fopen(str_join(name, '.', "s"), "r")) == NULL)
+            return NULL;
+
+    return in;
+}
+
 int main(int argc, char const *argv[])
 {
     FILE *in = NULL;
-    FILE *out = NULL;
     char *name = NULL;
+    parser_t *rc = NULL;
+    const char *msg = NULL;
 
     if (argc < 2 || (name = get_name(argv[1])) == NULL)
         return EXIT_ERROR;
-    if ((in = fopen(name, "r")) == NULL)
-        if ((in = fopen(str_join(name, '.', "s"), "r")) == NULL)
-            return EXIT_ERROR;
-    if ((out = fopen(str_join(name, '.', "cor"), "wb")) == NULL)
+    if ((in = get_input(name)) == NULL)
         return EXIT_ERROR;
-    if (redcode_encode(in, out) < 0)
-        return EXIT_ERROR;
+    if ((rc = redcode_parse(in)) == NULL || redcode_check(rc, &msg) < 0) {
+        if (msg != NULL)
+            my_puts(msg);
 
+        return EXIT_ERROR;
+    }
+    if (redcode_setoutput(rc, fopen(str_join(name, '.', "cor"), "wb")) < 0)
+        return EXIT_ERROR;
+    if (redcode_encode(rc) < 0)
+        return EXIT_ERROR;
     return EXIT_SUCCESS;
 }
